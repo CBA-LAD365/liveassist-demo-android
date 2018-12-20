a28b3f109eaba26eb4f87bc5c2895970c317c469
phillipkillen <pkillen@cafex.com>
2018-12-20 07:38:42 +0000

    MD-4114 converted to use android-x libraries

--- app/src/main/java/com/cafex/liveassist_android_demo/MainActivity.java
+++ app/src/main/java/com/cafex/liveassist_android_demo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.cafex.liveassist_android_demo;
 
 import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
